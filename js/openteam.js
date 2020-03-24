49684dc4a5705450e9a56bcafb8c94554a6916eb
Evgenii Konkov <evgeniikonkov1@gmail.com>
2020-03-24 15:16:30 +1030

    openteam js added

---
+++ js/openteam.js
@@ -0,0 +1,34 @@
+function openteam() {
+
+    const team = document.querySelector('.team__list');
+    const member = document.querySelectorAll('.team__name');
+
+    console.log(team);
+    console.log(member);
+
+    team.addEventListener('click', function(e) {
+
+    if (e.target.parentNode.classList.contains('team__item--active')) {
+        e.target.parentNode.classList.remove('team__item--active')
+    } else {
+        for (let item of member) {
+
+            if (item.parentNode.classList.contains('team__item--active')) {
+                item.parentNode.classList.remove('team__item--active')
+            };
+
+        }
+
+        e.target.parentNode.classList.add('team__item--active');
+        console.log(e.target.parentNode);
+        
+    };
+
+
+      
+
+    });
+
+};
+
+openteam();
