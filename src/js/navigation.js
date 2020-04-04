175bb94e7b1e348f80e1b5e23d7ea906c4b7fb3f
Evgenii Konkov <evgeniikonkov1@gmail.com>
2020-04-04 21:03:59 +1030

    onepage scroll

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,4 +1,4 @@
-$ (function(){
+/* $ (function(){
 
     const pagination = $(('.pagination'), $('.wrapper')),
         dots = pagination.find('.pagination__item'),
@@ -94,6 +94,119 @@ $ (function(){
  */
 
  
-    });
+/*     });
+
+}); */ 
+
+
+const sections = $('.section');
+const display = $('.maincontent');
+const pagination = $('.pagination__item');
+const activemenu = $('.header-menu__list');
+const paginationDots = $('.pagination__dot');
+const paginationDotsActive = $('.pagination__dot:before');
+const md = new MobileDetect(window.navigator.userAgent);
+const isMobile = md.mobile();
+
+$(window).on('wheel' , e => {
+    const deltaY = e.originalEvent.deltaY;
+
+    if( deltaY > 0 ) {
+        scrollSection('next');
+    };
+    if( deltaY < 0 ) {
+        scrollSection('prev');
+    };
+});
+let inScroll = false; //the section is not moving now
+
+const performTransition = newSectionNumber => {
+
+    if(inScroll == false) {
+        inScroll = true; // the section is moving now
+        const newPositionNumber = newSectionNumber* -100;
+        const newPositionString = newPositionNumber + "%";
+        sections.eq(newSectionNumber).addClass('active').siblings().removeClass('active')
+        display.css({
+            transform: `translateY(${newPositionNumber}%)`,
+        });
+        activemenu.css({
+            transform: `translateY(${-newPositionNumber}%)`,
+        });
+        setTimeout(() => {
+            inScroll = false;
+            paginationColor(newSectionNumber);
+            pagination.eq(newSectionNumber).addClass('pagination__item--active').siblings().removeClass('pagination__item--active');
+        }, 1300);
+    }
+};
+
+const scrollSection = direction => {
+    const activeSection = sections.filter('.active');
+    const nextSection = activeSection.next();
+    const prevSection = activeSection.prev();
+    
+    if(nextSection.length && direction === 'next') {
+        performTransition(nextSection.index())
+    };
+    if(prevSection.length && direction === 'prev') {
+        performTransition(prevSection.index())
+    };
+};
+
+$(document).on('keydown', e => {
+    const tagName = e.target.tagName.toLowerCase();
+    const userTypingInInputs = tagName === "input" || tagName === "textarea";
+
+  if (userTypingInInputs) return;
+
+  switch (e.keyCode) {
+    case 38:
+      scrollSection('prev');
+      break;
+    case 40:
+      scrollSection('next');
+      break;
+  }
+});
+ const paginationColor = newSectionNumber => {
+     const whiteSectors = newSectionNumber === '2'|| newSectionNumber ==="6" || newSectionNumber === "8";
+
+     if(whiteSectors) {
+        paginationDots.css("background-color", '#626262');
+        paginationDotsActive.css("border-color", "#626262");
+    };
+    if(!whiteSectors) {
+       paginationDots.css("background-color", '#ffffff');
+       paginationDotsActive.css("border-color", "#626262");
+   };
+};
+//pagination and menu
+
+$('[data-scroll-to]').on('click', e => {
+    e.preventDefault();
+
+    const $this = $(e.currentTarget);
+    const newSectionNumber = $this.attr('data-scroll-to');
+
+    performTransition(newSectionNumber);
+    
+    pagination.eq(newSectionNumber).addClass('pagination__item--active').siblings().removeClass('pagination__item--active');
 
 });
+
+// toushswipe lib
+if(isMobile) {
+    $('body').swipe({
+        swipe:(event, direction) => {
+            let scrollDirection;
+            if(direction === "up") {
+                scrollDirection = 'next'
+            };
+            if(direction === "down") {
+                scrollDirection = 'prev'
+        };
+        scrollSection(scrollDirection);
+    }
+});
+}
