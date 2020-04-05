45902cdd607c52ad5a7b42b1573de8f55012146f
Evgenii Konkov <evgeniikonkov1@gmail.com>
2020-04-05 14:50:01 +0930

    pagination color changing corrected, mainmenu corrected

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,113 +1,13 @@
-/* $ (function(){
-
-    const pagination = $(('.pagination'), $('.wrapper')),
-        dots = pagination.find('.pagination__item'),
-        activeDot = dots.filter('.pagination__item--active');
-
-    function dotIndication(activeDot, requiredDot) {
-        activeDot.removeClass('pagination__item--active');
-        requiredDot.classList.add('pagination__item--active');
-
-    };
-
-    $('.header-menu__link').on('click', function(e) {
-
-        const menu = $(('.header-menu__list'), $('.wrapper')),
-            requiredSection = $(e.currentTarget.parentNode),
-            requiredSectionIndex = requiredSection.index(),
-
-            activeDotIndex = 0,
-            requiredDotIndex = requiredSectionIndex,
-            requiredDot = dots[requiredDotIndex];
-           
-        dotIndication(activeDot, requiredDot);
-    });
-
-    $('.page-control').on('click', function(e) {
-
-        const 
-            activeDotIndex = 0,
-            requiredDotIndex = 1,
-            requiredDot = dots[requiredDotIndex];
-        
-        dotIndication(activeDot, requiredDot);
-    });
-
-    $('.header__btn').on('click', function(e) {
-
-        const 
-            activeDotIndex = 0,
-            requiredDotIndex = 6,
-            requiredDot = dots[requiredDotIndex];
-       
-        dotIndication(activeDot, requiredDot);
-    });
-
-    $('.order__btn').on('click', function(e) {
-
-        const 
-        
-            activeDotIndex = 1,
-            requiredDotIndex = 6,
-            requiredDot = dots[requiredDotIndex];
-    
-        dotIndication(activeDot, requiredDot);
-    });
-
-
-   
-
-/*     $('.section').on('wheel', function(e) {
-       console.log('jksdbkj');
-       const activeSection = $(e.currentTarget);
-       console.log(activeSection);
-       const activeSectionIndex = activeSection.index() - 2;
-       console.log(activeSectionIndex);
-       let nextSectionIndex = activeSectionIndex;
-       const wheelDirection = e.originalEvent.deltaY;
-       const sectionHeightPx = activeSection.css('height');
-       const sectionHeight = parseFloat(sectionHeightPx);
-       console.log(sectionHeight);
-
-       if(wheelDirection > 0) {
-           nextSectionIndex +=1;
-           
-       } else {
-           nextSectionIndex -=1
-       }
-       if(nextSectionIndex !== -1) {
-       }
-       let scroll = document.querySelector('.wrapper');
-       if(-1 < nextSectionIndex && nextSectionIndex < 9) {
-        
-       console.log(nextSectionIndex);
-        let shift = -(nextSectionIndex * sectionHeight)+"px";
-        let translateY = 'translateY('+shift+')';
-        console.log(translateY);
-        console.log(shift);
-        console.log(scroll);
-        scroll.style.transform = translateY;
-         $('.wrapper').css({
-          transform: translateY('shift')
-         })
-        }
- */
-
- 
-/*     });
-
-}); */ 
-
 
 const sections = $('.section');
 const display = $('.maincontent');
 const pagination = $('.pagination__item');
 const activemenu = $('.header-menu__list');
 const paginationDots = $('.pagination__dot');
-const paginationDotsActive = $('.pagination__dot:before');
-const md = new MobileDetect(window.navigator.userAgent);
-const isMobile = md.mobile();
-
+const paginationDotsActive = document.querySelectorAll('.pagination__dot::before');
+//const md = new MobileDetect(window.navigator.userAgent);
+//const isMobile = md.mobile();
+console.log(paginationDotsActive);
 $(window).on('wheel' , e => {
     const deltaY = e.originalEvent.deltaY;
 
@@ -171,14 +71,16 @@ $(document).on('keydown', e => {
 });
  const paginationColor = newSectionNumber => {
      const whiteSectors = newSectionNumber === '2'|| newSectionNumber ==="6" || newSectionNumber === "8";
-
+     paginationDots.removeClass('pagination__dot--black');
      if(whiteSectors) {
-        paginationDots.css("background-color", '#626262');
-        paginationDotsActive.css("border-color", "#626262");
+        paginationDots.css("background-color", '#5e5e5e');
+        paginationDots.addClass('pagination__dot--black');
+        //paginationDotsActive.css("border-color", "#5e5e5e");
     };
     if(!whiteSectors) {
        paginationDots.css("background-color", '#ffffff');
-       paginationDotsActive.css("border-color", "#626262");
+
+       //paginationDotsActive.css("border-color", "#ffffff");
    };
 };
 //pagination and menu
@@ -195,8 +97,8 @@ $('[data-scroll-to]').on('click', e => {
 
 });
 
-// toushswipe lib
-if(isMobile) {
+// toushswipe and mobile-detect libs don't work
+/* if(isMobile) {
     $('body').swipe({
         swipe:(event, direction) => {
             let scrollDirection;
@@ -209,4 +111,4 @@ if(isMobile) {
         scrollSection(scrollDirection);
     }
 });
-}
+} */
